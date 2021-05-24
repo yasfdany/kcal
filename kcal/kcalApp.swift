a2712829426392f59d53ca502a4d50b1736c1fdc
Ya'suf Dany <yasfdany@gmail.com>
2021-05-24 18:41:59 +0700

    Initial Commit

---
+++ kcal/kcalApp.swift
@@ -0,0 +1,17 @@
+//
+//  kcalApp.swift
+//  kcal
+//
+//  Created by Ya'suf Dany on 24/05/21.
+//
+
+import SwiftUI
+
+@main
+struct kcalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
