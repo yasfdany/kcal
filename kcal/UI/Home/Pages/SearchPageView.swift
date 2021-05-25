d5edab2936a7ef234c5dd68722e6c137d0b3c06d
Ya'suf Dany <yasfdany@gmail.com>
2021-05-25 16:01:51 +0700

    Search view

--- kcal/UI/Home/Pages/SearchPageView.swift
+++ kcal/UI/Home/Pages/SearchPageView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct SearchPageView: View {
+    @State var searchText : String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0.0){
+                HStack{
+                    Image("Search")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:18)
+                    Spacer().frame(width: 14)
+                    TextField("Search recipes, articles, people...",text:$searchText)
+                        .font(.custom("Nunito-Regular", size: 18))
+                    Spacer()
+                }
+                .padding(24)
+                .background(Color(.black).opacity(0.05))
+                .cornerRadius(12)
+                .padding()
+                
+                Text("Hot Now")
+                    .font(.custom("Nunito-Bold", size: 22))
+                    .padding(.leading, 14)
+                    .padding(.top,14)
+                
+                ScrollView(.horizontal,showsIndicators: false){
+                    HStack(spacing: 16){
+                        Spacer().frame(width:4)
+                        ItemArticleView(image: "Pumkin")
+                        ItemArticleView(image: "Letuce")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ItemArticleView : View {
+    var image: String
+    let screenBound = UIScreen.main.bounds
+    
+    var body : some View{
+        VStack(alignment: .leading){
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width:screenBound.width * 0.56, height: 160)
+                .cornerRadius(0)
+            Text("The Pumkins Secrets")
+                .font(.custom("Nunito-Bold", size: 18))
+                .padding(.leading,12)
+            Text("Lorem ipsum dolor sit amet ipsum dolor sit amet")
+                .font(.custom("Nunito-Regular", size: 16))
+                .padding(.leading,12)
+            Spacer()
+        }
+        .background(Color.white)
+        .frame(width:screenBound.width * 0.56, height: 240)
+        .cornerRadius(18)
+        .shadow(color: Color.black.opacity(0.1),radius: 12)
+        .padding(.top)
+        .padding(.bottom)
     }
 }
 
